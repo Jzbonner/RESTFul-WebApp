12bde581f419d566fc688581ce9db035075dcd9e
Jzbonner <jarrett.bonner@gmail.com>
2018-09-16 03:14:32 -0400

    changes to AjaxEnv

--- AjaxEnv/app.js
+++ AjaxEnv/app.js
@@ -1,8 +1,22 @@
+/* 
+  TERMINAL INFORMATION 
+
+    ** Termination of Port 3000 cmd **
+    netstat -ano | findstr :3000 
+    taskkill /pid "PID" 
+    
+ */
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+// Database Setup 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/AjaxEnv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +32,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// DB router accessibility 
+app.use(function(req, res, next) {
+  req.db = db; 
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
